fe3e34841fe6849893885afbe6254aaa5bf33aa2
leshka290 <lleesshhkkaa00@gmail.com>
2023-05-02 11:19:49 +0300

    Доработка ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        checkException("alex@", "password", "password");
+        checkException("alex", "password", "password");
     }
 
     public static void checkException(String login, String password, String confirmPassword) {
         try {
-            WrongLoginException.isValid(login);
-            WrongPasswordException.isValid(password, confirmPassword);
+            if(login.length() > 20){
+                throw new WrongLoginException("Слишком большая длинна логина");
+            }
+            if(password.length() > 20 || !(password.equals(confirmPassword))) {
+                throw new WrongPasswordException("Слишком большая длинна пароля или пароли не совпадают");
+            }
         } catch (WrongLoginException | WrongPasswordException e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
     }
 }
